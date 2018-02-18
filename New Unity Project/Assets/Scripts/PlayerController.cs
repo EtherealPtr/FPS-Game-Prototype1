082d26bbf7e01846349998d395e45acd66788f7d
Rony Hanna <RonyHannaSE@hotmail.com>
2018-02-19 12:14:09 +1300

    Player Movement and Rotation
    Player object (capsule) rotates horizontally. Player's camera rotates vertically.

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -3,16 +3,37 @@
 [RequireComponent(typeof(PlayerMotor))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private float playerSpeed = 5.0f;
+
+    [SerializeField]
+    private float mouseSensitivity = 3.0f;
+
     PlayerMotor motor;
 
     private void Start()
     {
-
+        motor = GetComponent<PlayerMotor>();
     }
 
     private void Update()
     {
+        float m_xPos = Input.GetAxisRaw("Horizontal");
+        float m_zPos = Input.GetAxisRaw("Vertical");
 
+        Vector3 moveHorizontal = transform.right * m_xPos;
+        Vector3 moveVertical = transform.forward * m_zPos;
+
+        // Calculate velocity
+        Vector3 velocity = (moveHorizontal + moveVertical).normalized * playerSpeed;
+        motor.MovePlayer(velocity);
+
+        float m_yRot = Input.GetAxisRaw("Mouse X");
+        Vector3 rotation = new Vector3(0.0f, m_yRot, 0.0f) * mouseSensitivity;
+        motor.RotatePlayer(rotation);
+
+        float m_xRot = Input.GetAxisRaw("Mouse Y");
+        Vector3 camRot = new Vector3(m_xRot, 0.0f, 0.0f) * mouseSensitivity;
+        motor.RotateCamera(camRot);
     }
-
 }
--- New Unity Project/Assets/Scripts/PlayerMotor.cs
+++ New Unity Project/Assets/Scripts/PlayerMotor.cs
@@ -2,16 +2,69 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMotor : MonoBehaviour
+{
+    Vector3 velocity = Vector3.zero;
+    Vector3 rotation = Vector3.zero;
+    Vector3 camRot = Vector3.zero;
 
-public class PlayerMotor : MonoBehaviour {
+    [SerializeField]
+    private Camera playerCam;
+
+    Rigidbody rb;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame 
-	void Update () {
-		
-	}
+	// Update is called once per physics frame 
+	void FixedUpdate ()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    // Function that gets the player's velocity from the PlayerController script
+    public void MovePlayer(Vector3 _vel)
+    {
+        velocity = _vel;
+    }
+
+    // Function that rotates the "player object" horizontally 
+    public void RotatePlayer(Vector3 _rot)
+    {
+        rotation = _rot;
+    }
+
+    // Function that rotates the "camera" vertically 
+    public void RotateCamera(Vector3 _rot)
+    {
+        camRot = _rot;
+    }
+
+    // Function that performs movement every frame
+    void PerformMovement()
+    {
+        if (velocity != null)
+        {
+            rb.MovePosition(transform.position + velocity * Time.deltaTime);
+        }
+    }
+
+    // Function that perform rotation every frame
+    void PerformRotation()
+    {
+        if (rotation != null)
+        {
+            rb.MoveRotation(transform.rotation * Quaternion.Euler(rotation));
+        }
+
+        if (playerCam != null)
+        {
+            playerCam.transform.Rotate(-camRot);
+        }
+    }
 }

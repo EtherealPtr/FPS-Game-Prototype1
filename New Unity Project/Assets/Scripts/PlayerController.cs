9aa68336eb2c2edac6491506b134bc1f0c588548
Rony Hanna <RonyHannaSE@hotmail.com>
2018-02-19 11:23:31 +1300

    First push

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    PlayerMotor motor;
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+}

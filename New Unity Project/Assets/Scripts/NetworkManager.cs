f31a3e448fe156653a594a0352553bfeef21996f
Rony Hanna <RonyHannaSE@hotmail.com>
2018-02-19 13:59:34 +1300

    Networked movement and rotation

--- New Unity Project/Assets/Scripts/NetworkManager.cs
+++ New Unity Project/Assets/Scripts/NetworkManager.cs
@@ -1,16 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Networking;
 
-public class NetworkManager : MonoBehaviour {
+public class NetworkManager : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    Camera sceneCamera;
+
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentsToDisable.Length; ++i)
+            {
+                componentsToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+            }
+        }
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+        }
+    }
 }
